d213abf14fcd0c153d023fcaf97b04abda7e8ab8
p1lloww <santosrobson635@gmail.com>
2025-07-17 23:04:48 -0300

    finalização da atividade

--- src/App.jsx
+++ src/App.jsx
@@ -47,148 +47,163 @@ function App() {
   });
 
   return (
+    <div>
     <div className="App">
-      <div>
-        <form onSubmit={formik.handleSubmit}>
+      <header>SPACE🛸</header>
+      <div className="formulario">
+        <form onSubmit={formik.handleSubmit} className="formulario-em-si">
           <fieldset>
-            <div>
-              <label>Nome</label>
-              <input
-                type="text"
-                value={formik.values.nome}
-                name="nome"
-                onChange={formik.handleChange}
-              />
-            </div>
+            <div className="itens-formulario">
+              <div>
+                <label>Nome</label>
+                <input
+                  type="text"
+                  value={formik.values.nome}
+                  name="nome"
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>Email</label>
-              <input
-                type="text"
-                value={formik.values.email}
-                name="email"
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>Email</label>
+                <input
+                  type="text"
+                  value={formik.values.email}
+                  name="email"
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>Rua</label>
-              <input
-                type="text"
-                value={formik.values.rua}
-                name="rua"
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>Rua</label>
+                <input
+                  type="text"
+                  value={formik.values.rua}
+                  name="rua"
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>Bairro</label>
-              <input
-                type="text"
-                value={formik.values.bairro}
-                name="bairro"
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>Bairro</label>
+                <input
+                  type="text"
+                  value={formik.values.bairro}
+                  name="bairro"
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label htmlFor="profissao">Profissão</label>
-              <select
-                name="profissao"
-                value={formik.values.profissao}
-                onChange={formik.handleChange}
-              >
-                <option value="">escolha uma opção</option>
-                <option value="educação basica">educação básica</option>
-                <option value="ensino superior">ensino superior</option>
-                <option value="pos graduacao">pós-graduação</option>
-              </select>
-            </div>
+              <div>
+                <label htmlFor="profissao">Profissão</label>
+                <select
+                  name="profissao"
+                  value={formik.values.profissao}
+                  onChange={formik.handleChange}
+                >
+                  <option value="">escolha uma opção</option>
+                  <option value="educação basica">educação básica</option>
+                  <option value="ensino superior">ensino superior</option>
+                  <option value="pos graduacao">pós-graduação</option>
+                </select>
+              </div>
 
-            <div>
-              <label htmlFor="planeta">Planeta que quer visitar</label>
-              <select
-                name="planeta"
-                value={formik.values.planeta}
-                onChange={formik.handleChange}
-              >
-                <option value="">escolha uma opção</option>
-                <option value="mercurio">Mercúrio</option>
-                <option value="venus">Vênus</option>
-                <option value="terra">Terra</option>
-                <option value="marte">Marte</option>
-                <option value="jupiter">Júpiter</option>
-                <option value="saturno">Saturno</option>
-                <option value="urano">Urano</option>
-                <option value="netuno">Netuno</option>
-              </select>
-            </div>
+              <div>
+                <label htmlFor="planeta">Planeta que quer visitar</label>
+                <select
+                  name="planeta"
+                  value={formik.values.planeta}
+                  onChange={formik.handleChange}
+                >
+                  <option value="">escolha uma opção</option>
+                  <option value="mercurio">Mercúrio</option>
+                  <option value="venus">Vênus</option>
+                  <option value="terra">Terra</option>
+                  <option value="marte">Marte</option>
+                  <option value="jupiter">Júpiter</option>
+                  <option value="saturno">Saturno</option>
+                  <option value="urano">Urano</option>
+                  <option value="netuno">Netuno</option>
+                </select>
+              </div>
 
-            <div>
-              <label>Telefone</label>
-              <input
-                type="text"
-                name="telefone"
-                value={formik.values.telefone}
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>Telefone</label>
+                <input
+                  type="text"
+                  name="telefone"
+                  value={formik.values.telefone}
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>CEP</label>
-              <input
-                type="text"
-                name="cep"
-                value={formik.values.cep}
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>CEP</label>
+                <input
+                  type="text"
+                  name="cep"
+                  value={formik.values.cep}
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>Casa</label>
-              <input
-                type="text"
-                name="casa"
-                value={formik.values.casa}
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>Casa</label>
+                <input
+                  type="text"
+                  name="casa"
+                  value={formik.values.casa}
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <label>CPF</label>
-              <input
-                type="text"
-                name="cpf"
-                value={formik.values.cpf}
-                onChange={formik.handleChange}
-              />
-            </div>
+              <div>
+                <label>CPF</label>
+                <input
+                  type="text"
+                  name="cpf"
+                  value={formik.values.cpf}
+                  onChange={formik.handleChange}
+                />
+              </div>
 
-            <div>
-              <input
-                type="checkbox"
-                name="riscos"
-                checked={formik.values.riscos}
-                onChange={formik.handleChange}
-              />
-              <label>Você aceita os riscos da viagem?</label>
-            </div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="riscos"
+                  checked={formik.values.riscos}
+                  onChange={formik.handleChange}
+                />
+                <label>Você aceita os riscos da viagem?</label>
+              </div>
 
-            <div>
-              <input
-                type="checkbox"
-                name="saude"
-                checked={formik.values.saude}
-                onChange={formik.handleChange}
-              />
-              <label>Você está com boa saúde?</label>
-            </div>
+              <div>
+                <input
+                  type="checkbox"
+                  name="saude"
+                  checked={formik.values.saude}
+                  onChange={formik.handleChange}
+                />
+                <label>Você está com boa saúde?</label>
+              </div>
 
-            <input type="submit" value="submit" />
+              <input type="submit" value="submit" className="submit-css" />
+            </div>
           </fieldset>
         </form>
       </div>
+
     </div>
+
+      <footer>
+        <h2>
+        Facebook
+        Instagram
+        X
+        Youtube
+        </h2>
+      </footer>
+      </div>
   );
 }
 
